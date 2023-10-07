cec59ad72123d48b14b28ebf64e1a145c244be64
Cristian Priceputu <priceputu.cristian@gmail.com>
2023-10-07 11:09:33 +0300

    Added some js and css.

--- template_starlett/app.py
+++ template_starlett/app.py
@@ -2,5 +2,10 @@
 from starlette.applications import Starlette
 import settings
 import routes
+import uvicorn
+
 
 app = Starlette(debug=settings.DEBUG, routes=routes.routes)
+
+if __name__ == '__main__':
+    uvicorn.run(app=app, port=8000)
